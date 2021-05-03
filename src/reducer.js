f14bb39255b249d01acfecde64589bc45a3ef91a
Joseph M <jmedina1985@hotmail.com>
2021-05-02 22:45:16 -0700

    Setting up basic structure

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
+} from './actions';
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {};
+export default reducer;
