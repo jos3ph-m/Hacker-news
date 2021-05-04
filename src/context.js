5dc57d0c7fd3e71fb933fc0ed13f48887db57a6c
Joseph M <jmedina1985@hotmail.com>
2021-05-03 22:15:37 -0700

    Fetch stories

--- src/context.js
+++ src/context.js
@@ -35,7 +35,7 @@ const AppProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}`);
   }, []);
   return (
     <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
