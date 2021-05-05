55039501eb770240315630ed69c988707ccb4c2c
Joseph M <jmedina1985@hotmail.com>
2021-05-04 21:03:15 -0700

    Fetch stories

--- src/context.js
+++ src/context.js
@@ -28,7 +28,6 @@ const AppProvider = ({ children }) => {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
     } catch (error) {
       console.log(error);
     }
