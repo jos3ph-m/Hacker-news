1d80dc2fa0d38ee2176279ce1cc1247ae282315a
Joseph M <jmedina1985@hotmail.com>
2021-05-07 21:41:16 -0700

    Handle search

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -6,6 +6,7 @@ const SearchForm = () => {
   return (
     <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>search hacker news</h2>
+      <input type="text" className="form-input" />
     </form>
   );
 };
