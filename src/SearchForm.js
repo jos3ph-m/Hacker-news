1647177e9dc417fc117683d458ecd573f53ecf06
Joseph M <jmedina1985@hotmail.com>
2021-05-07 21:40:03 -0700

    Handle search

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,7 +3,11 @@ import { useGlobalContext } from './context';
 
 const SearchForm = () => {
   const { query, handleSearch } = useGlobalContext();
-  return <form>search form</form>;
+  return (
+    <form>
+      <h2>search hacker news</h2>
+    </form>
+  );
 };
 
 export default SearchForm;
