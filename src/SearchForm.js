3d56085790808b3fc4ae50a061baf3d218d55c10
Joseph M <jmedina1985@hotmail.com>
2021-05-07 21:39:28 -0700

    Remove story

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
 const SearchForm = () => {
-  return <h2>search form</h2>
-}
+  const { query, handleSearch } = useGlobalContext();
+  return <h2>search form</h2>;
+};
 
-export default SearchForm
+export default SearchForm;
