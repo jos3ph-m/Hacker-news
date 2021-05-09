1d836b45f5a1008085a597dd1dd30051a5272d9b
Joseph M <jmedina1985@hotmail.com>
2021-05-08 21:35:14 -0700

    Prev and next buttons

--- src/Buttons.js
+++ src/Buttons.js
@@ -3,7 +3,11 @@ import { useGlobalContext } from './context';
 
 const Buttons = () => {
   const { isLoading, page, nbPages, handlePage } = useGlobalContext();
-  return <div className="btn-container">button container</div>;
+  return (
+    <div className="btn-container">
+      <button onClick={handlePage}></button>
+    </div>
+  );
 };
 
 export default Buttons;
